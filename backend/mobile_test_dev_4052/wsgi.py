29203adcc636b0e29f14e689e65e96a5181467a7
Crowdbotics <team@crowdbotics.com>
2020-05-02 11:46:15 +0000

    Update models

--- backend/mobile_test_dev_4052/wsgi.py
+++ backend/mobile_test_dev_4052/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile_test_dev_4052.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobile_test_dev_4052.settings")
 
 application = get_wsgi_application()
